a3f076f2f3537e05636cda8a6a49146f867ab1d3
et120 <e.trotter120@gmail.com>
2024-02-23 09:53:17 -0800

    ch 5 complete

--- rps.py
+++ rps.py
@@ -9,6 +9,19 @@
 # import modules
 import sys
 import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+# const data - all caps
+
+# print(RPS(1))
+# print(RPS.ROCK)
+# print(RPS['ROCK'])
+# print(RPS.ROCK.value)
+# sys.exit()
 
 # Game Start
 print("")
@@ -23,7 +36,24 @@ computer_choice = random.choice("123")
 
 computer = int(computer_choice)
 
+# print("")
+# print("You chose " + player_choice + ".")
+# print("Python chose " + computer_choice + ".")
+# print("")
 print("")
-print("You chose " + player_choice + ".")
-print("Python chose " + computer_choice + ".")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
 print("")
+
+if player == 1 and computer == 3:
+    print("🎉 You win!")
+elif player == 2 and computer == 1:
+    print("🎉 You win!")
+elif player == 3 and computer == 2:
+    print("🎉 You win!")
+elif player == computer:
+    print("😲 Tie game!")
+else:
+    print("🐍 Python wins!")
+    
+# Windows Key + . => emojis
