07e08a7ecccf0dfac26777adfdf0577666331e07
et120 <e.trotter120@gmail.com>
2024-02-24 14:09:20 -0800

    ch 15 complete

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ This project is a 9+ hour "follow-along" to a freeCodeCamp YouTube `tutorial ser
 | 11 | :white_check_mark: | Scope | [scope.py](/chapters/scope.py), [rps4.py](/rps_game/rps4.py) | `global`, `nonlocal`, Refactor RPS |
 | 12 | :white_check_mark: | Closures | [closure.py](/chapters/closure.py), [rps5.py](/rps_game/rps5.py) | Closure, Refactor RPS |
 | 13 | :white_check_mark: | f-Strings | [f-strings.py](/chapters/f_strings.py), [rps6.py](/rps_game/rps6.py)  | f-Strings, Old Methods |
-| 14 | :white_check_mark: | Modules | [modules.py](/chapters/modules.py) | Modules, Refactor RPS |
-| 15 | :white_square_button: | Command Line Arguments |  |  |
-| 16 | :white_square_button: | Challengs |  |  |
+| 14 | :white_check_mark: | Modules | [modules.py](/chapters/modules.py), [kansas.py](/chapters/kansas.py), [rps7.py](/rps_game/rps7.py) | Modules, Refactor RPS |
+| 15 | :white_check_mark: | Command Line Arguments | [hello_person.py](/chapters/hello_person.py), [rps8.py](/rps_game/rps8.py) | Command Line Arguments |
+| 16 | :white_square_button: | Challenges |  |  |
 | 17 | :white_square_button: | Lambda & Higher Order Functions |  |  |
 | 18 | :white_square_button: | Classes & Objects |  |  |
 | 19 | :white_square_button: | Exceptions & Errors |  |  |
---
+++ rps_game/rps8.py
@@ -0,0 +1,102 @@
+import sys
+import random
+from enum import Enum
+
+def rps(name='PlayerOne'):
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
+
+    def play_rps():
+        nonlocal name
+        nonlocal player_wins
+        nonlocal python_wins
+        
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
+
+        player_choice = input(f"\n{name}, please enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n")
+
+        # Use recursive function here
+        if player_choice not in ["1","2","3"]:
+            print(f"{name}, please enter 1, 2, or 3.")
+            return play_rps()
+        
+        player = int(player_choice)
+            
+        computer_choice = random.choice("123")
+
+        computer = int(computer_choice)
+
+        print(f"\n{name}, you chose {str(RPS(player)).replace('RPS.', '').title()}.")
+        print(f"Python chose {str(RPS(computer)).replace('RPS.', '').title()}.")
+
+        def decide_winner(player, computer):
+            nonlocal name
+            nonlocal player_wins
+            nonlocal python_wins
+            
+            if player == 1 and computer == 3:
+                player_wins += 1
+                return f"🎉 {name}, you win!"
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return f"🎉 {name}, you win!"
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return f"🎉 {name}, you win!"
+            elif player == computer:
+                return "😲 Tie game!"
+            else:
+                python_wins += 1
+                return "🐍 Python wins!\nSorry, {name} 🥲"
+
+        game_result = decide_winner(player, computer)
+        print(game_result)
+        
+        nonlocal game_count
+        game_count += 1
+        
+        print(f"\nGame Count: {game_count}")
+        print(f"\n{name}'s Wins: {player_wins}")
+        print(f"\nPython Wins: {python_wins}")
+        
+        print(f"\nPlay again, {name}?")
+        
+        while True:
+            playagain = input("\nY for Yes or \nQ to Quit\n")
+            if playagain.lower() not in ["y","q"]:
+                continue
+            else:
+                break
+        
+        if playagain.lower() == "y":
+            return play_rps()
+        else:
+            print("\n🥳🥳🥳🥳")
+            print("Thank you for playing!\n")
+            sys.exit(f"Bye, {name}! 👋")
+            
+    return play_rps
+            
+
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Provides a personalized game experience."
+    )
+
+    parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person playing the game."
+    )
+    
+    args = parser.parse_args()
+    
+    rock_paper_scissors = rps(args.name)
+    rock_paper_scissors()
